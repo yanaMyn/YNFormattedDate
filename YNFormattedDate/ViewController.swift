313d5296f7c0f85a162e1489dfc058cbec8680a3
yana mulyana <fulan@macbook-pro-yana.local>
2018-06-25 13:11:59 +0700

    Initial Commit

---
+++ YNFormattedDate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YNFormattedDate
+//
+//  Created by yana mulyana on 25/06/18.
+//  Copyright © 2018 yana mulyana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
